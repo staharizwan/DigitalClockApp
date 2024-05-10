68926f8cf18a94fc36faa9f95a9a0470446a8faa
staharizwan <s.taha.rizwan@gmail.com>
2024-05-10 12:20:33 +0200

    Commit 4

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function updateClock() {
   const now = new Date();
-  const hours = now.getHours().toString().padStart(2, '0');
+  const hours = (now.getHours()).toString().padStart(2, '0');
   const seconds = now.getSeconds().toString().padStart(2, '0');
   const minutes = now.getMinutes().toString().padStart(2, '0');
   const timeString = `${hours}:${minutes}:${seconds}`;
